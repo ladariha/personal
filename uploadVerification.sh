e029a77c25dc25a1a0534c3c3c8246ac1ab28e3e
Lada Riha <riha.vladimir@gmail.com>
2017-10-20 13:27:42 +0200

    Update uploadVerification.sh

--- uploadVerification.sh
+++ uploadVerification.sh
@@ -1,3 +1,3 @@
-rm -rf /tmp/2017-04-01.csv
-hadoop fs -copyToLocal hdfs:/user/test201710190553542/2017-04-01.csv /tmp/
-md5sum /tmp/2017-04-01.csv
+rm -rf /tmp/bdm-fileuploadtest-0.1.0-SNAPSHOT.csv
+hadoop fs -copyToLocal hdfs:/tmp/bdm-fileuploadtest-0.1.0-SNAPSHOT.csv /tmp/
+md5sum /tmp/bdm-fileuploadtest-0.1.0-SNAPSHOT.csv
